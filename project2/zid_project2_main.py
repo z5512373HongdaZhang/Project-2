7473fe4a1c4b23406519ab5d978e95f023c93e8d
z5512373HongdaZhang <secular1118@gmail.com>
2024-04-19 14:54:39 +1000

    git log update

--- project2/zid_project2_main.py
+++ project2/zid_project2_main.py
@@ -408,7 +408,99 @@ if __name__ == "__main__":
 # .......
 #
 # Please replace the """?""" with your team's project 2 git log:
-git_log = """?"""
+git_log = """
+0bb6a76	z5512373HongdaZhang	Fri Apr 19 14:20:54 2024 +1000	Merge remote-tracking branch 'origin/main'
+3b6858b	z5512373HongdaZhang	Fri Apr 19 14:20:38 2024 +1000	Presentation update
+b7bfec0	z5512373HongdaZhang	Fri Apr 19 14:12:57 2024 +1000	Delete toolkit_config.py
+9ddf786	z5512373HongdaZhang	Fri Apr 19 14:05:04 2024 +1000	Presentation link update
+0ee0f74	Ziheng Rao	Thu Apr 18 16:39:18 2024 +1000	for testing
+a8fb59a	Ziheng Rao	Thu Apr 18 11:42:12 2024 +1000	part 8
+ea12bd9	z5512074DanWu	Sun Apr 14 23:17:19 2024 +1000	fill
+0742d36	z5512074DanWu	Sun Apr 14 22:56:18 2024 +1000	fill
+9ba5f41	z5512074DanWu	Sun Apr 14 22:53:42 2024 +1000	fill
+529104e	z5512373HongdaZhang	Sun Apr 14 14:24:13 2024 +1000	Q10 update
+627867e	z5512373HongdaZhang	Sun Apr 14 14:23:02 2024 +1000	Q9 update
+d40111e	z5512373HongdaZhang	Sun Apr 14 13:57:11 2024 +1000	Q10
+1c9f0af	z5512373HongdaZhang	Sun Apr 14 13:53:34 2024 +1000	Merge remote-tracking branch 'origin/main'
+551ab1e	z5512373HongdaZhang	Sun Apr 14 13:53:10 2024 +1000	Q9
+a7ee237	z5498046QianyuZhou	Sun Apr 14 13:29:50 2024 +1000	Q9
+91abd28	z5498046QianyuZhou	Sun Apr 14 13:16:42 2024 +1000	Q8
+e912574	Ziheng Rao	Sat Apr 13 19:18:48 2024 +1000	Merge remote-tracking branch 'origin/main' into main
+fb33711	Ziheng Rao	Sat Apr 13 19:18:09 2024 +1000	part 8
+1825a91	z5512074DanWu	Fri Apr 12 23:27:43 2024 +1000	simplified merge_tables
+e9abfd1	z5512074DanWu	Fri Apr 12 22:41:07 2024 +1000	Merge remote-tracking branch 'origin/main'
+7cb8020	z5512074DanWu	Fri Apr 12 22:40:43 2024 +1000	T-test
+ffae8be	z5512373HongdaZhang	Fri Apr 12 22:22:57 2024 +1000	Q9
+1949ba0	z5498046QianyuZhou	Fri Apr 12 22:19:59 2024 +1000	Merge remote-tracking branch 'origin/main'
+f0fc498	z5498046QianyuZhou	Fri Apr 12 22:19:39 2024 +1000	Q8
+543a29b	z5512074DanWu	Fri Apr 12 22:16:45 2024 +1000	test
+834ed40	z5512074DanWu	Fri Apr 12 22:15:24 2024 +1000	Merge remote-tracking branch 'origin/main'
+303ae85	z5512074DanWu	Fri Apr 12 22:14:42 2024 +1000	to_period(M)
+d130075	z5512074DanWu	Fri Apr 12 22:11:06 2024 +1000	to_period(M)
+2103c0f	z5498046QianyuZhou	Fri Apr 12 22:09:22 2024 +1000	modify cha
+ad583d2	z5512074DanWu	Fri Apr 12 22:06:04 2024 +1000	to_period(M)
+6ac572d	z5512074DanWu	Fri Apr 12 21:54:54 2024 +1000	to_period(M)
+a7ca129	z5512074DanWu	Fri Apr 12 21:52:28 2024 +1000	to_period(M)
+915003d	z5512074DanWu	Fri Apr 12 21:51:27 2024 +1000	to_period(M)
+2705eac	z5512074DanWu	Fri Apr 12 21:32:24 2024 +1000	to_period(M)
+fce335e	z5524321ShengfaSun	Fri Apr 12 20:12:01 2024 +1000	PARTIAL Q9
+72c5a6a	z5524321ShengfaSun	Fri Apr 12 20:09:56 2024 +1000	comments on 5.2
+a7f5d8a	z5524321ShengfaSun	Fri Apr 12 16:39:12 2024 +1000	Merge remote-tracking branch 'origin/main'
+e39ab22	z5512074DanWu	Fri Apr 12 13:44:48 2024 +1000	Modify Q1 to Q4
+4c0dad6	z5512074DanWu	Fri Apr 12 13:33:17 2024 +1000	Modify Q1 to Q4
+1dcd771	z5524321ShengfaSun	Thu Apr 11 22:31:55 2024 +1000	Merge remote-tracking branch 'origin/main'
+2e212ed	z5498046QianyuZhou	Thu Apr 11 22:21:21 2024 +1000	8.Q7
+b4263b6	z5524321ShengfaSun	Thu Apr 11 21:15:24 2024 +1000	Merge remote-tracking branch 'origin/main'
+259254c	Ziheng Rao	Thu Apr 11 21:12:40 2024 +1000	part 8
+5f255f9	Ziheng Rao	Thu Apr 11 20:32:37 2024 +1000	part 8
+68de599	z5524321ShengfaSun	Thu Apr 11 19:38:32 2024 +1000	Merge remote-tracking branch 'origin/main'
+2521a8b	Ziheng Rao	Thu Apr 11 19:21:31 2024 +1000	part 8
+bdd72ce	z5524321ShengfaSun	Thu Apr 11 17:01:41 2024 +1000	comments on 5.2
+0c0c577	z5524321ShengfaSun	Thu Apr 11 16:48:43 2024 +1000	Merge remote-tracking branch 'origin/main'
+145cbaf	z5524321ShengfaSun	Thu Apr 11 16:47:32 2024 +1000	fixing
+e3281af	Ziheng Rao	Thu Apr 11 16:19:28 2024 +1000	Merge remote-tracking branch 'origin/main' into main
+0a9cb65	z5512074DanWu	Thu Apr 11 16:12:03 2024 +1000	Comments
+7e5701f	Ziheng Rao	Thu Apr 11 16:00:55 2024 +1000	Merge remote-tracking branch 'origin/main' into main
+246f615	Ziheng Rao	Thu Apr 11 15:59:34 2024 +1000	part 8
+798157e	Ziheng Rao	Thu Apr 11 15:44:44 2024 +1000	Part 7
+2a62fd5	z5512074DanWu	Thu Apr 11 15:30:38 2024 +1000	auxiliary functions
+a18109c	z5512074DanWu	Thu Apr 11 15:26:01 2024 +1000	auxiliary functions
+6484304	z5512074DanWu	Thu Apr 11 14:57:28 2024 +1000	merge_tables
+1e9351f	z5512074DanWu	Thu Apr 11 12:06:45 2024 +1000	Merge remote-tracking branch 'origin/main'
+e0f76db	z5512074DanWu	Thu Apr 11 11:58:46 2024 +1000	Modified vol_cal
+55305e4	Ziheng Rao	Thu Apr 11 09:16:12 2024 +1000	输入
+2320f86	Ziheng Rao	Thu Apr 11 02:36:05 2024 +1000	1
+fd07e5a	Ziheng Rao	Thu Apr 11 02:30:57 2024 +1000	Merge remote-tracking branch 'origin/main' into main
+7033c0b	z5512074DanWu	Thu Apr 11 00:35:12 2024 +1000	Merge remote-tracking branch 'origin/main'
+ca05eab	z5524321ShengfaSun	Thu Apr 11 00:32:19 2024 +1000	characteristics updated
+b29cc1e	z5524321ShengfaSun	Thu Apr 11 00:27:35 2024 +1000	characteristics updated
+567ac46	z5512074DanWu	Thu Apr 11 00:10:54 2024 +1000	5.1
+26c6487	z5498046QianyuZhou	Wed Apr 10 21:25:00 2024 +1000	4doublecheck
+f4c7fcc	z5512373HongdaZhang	Wed Apr 10 21:11:43 2024 +1000	test new
+638f547	Hongda Zhang	Wed Apr 10 20:44:43 2024 +1000	Part4 Complete
+12d8729	z5512373HongdaZhang	Wed Apr 10 20:40:33 2024 +1000	test
+96148ef	Hongda Zhang	Wed Apr 10 20:38:17 2024 +1000	test
+9a4d25b	Hongda Zhang	Wed Apr 10 19:51:52 2024 +1000	4.5 develop
+ae3d781	z5498046QianyuZhou	Wed Apr 10 19:36:50 2024 +1000	4.4develop
+77c0811	z5498046QianyuZhou	Wed Apr 10 19:35:39 2024 +1000	4.3develop
+863401b	Hongda Zhang	Wed Apr 10 18:24:23 2024 +1000	4.3 code
+62c507d	z5498046QianyuZhou	Wed Apr 10 18:07:30 2024 +1000	4.2develop
+28f9f53	z5498046QianyuZhou	Wed Apr 10 17:44:44 2024 +1000	4.2 code
+5fd5065	z5498046QianyuZhou	Wed Apr 10 17:43:59 2024 +1000	test new
+b5d2667	Ziheng Rao	Wed Apr 10 17:17:01 2024 +1000	1
+85c3328	Hongda Zhang	Wed Apr 10 13:28:51 2024 +1000	Part 2 import
+cd9a9e9	z5524321ShengfaSun	Wed Apr 10 12:41:15 2024 +1000	Merge remote-tracking branch 'origin/main'
+26ff229	z5524321ShengfaSun	Wed Apr 10 12:40:57 2024 +1000	char
+549f910	z5498046QianyuZhou	Wed Apr 10 12:38:13 2024 +1000	Merge remote-tracking branch 'origin/main'
+3e07146	z5512074DanWu	Wed Apr 10 12:37:36 2024 +1000	Merge remote-tracking branch 'origin/main'
+3f64c92	z5498046QianyuZhou	Wed Apr 10 12:37:34 2024 +1000	test new
+e8eb8b1	z5512074DanWu	Wed Apr 10 12:37:02 2024 +1000	test
+f8f11b6	zid5498046QianyuZhou	Wed Apr 10 12:32:06 2024 +1000	test
+a7c8bf1	z5512373HongdaZhang	Wed Apr 10 12:12:16 2024 +1000	Add files via upload
+f741e95	z5512373HongdaZhang	Wed Apr 10 11:32:31 2024 +1000	Add files via upload
+0690165	z5512373HongdaZhang	Wed Apr 10 11:31:12 2024 +1000	Delete project2.zip
+3d12dc9	z5512373HongdaZhang	Wed Apr 10 11:30:40 2024 +1000	Add files via upload
+"""
 
 # ----------------------------------------------------------------------------
 # Part 11: project 2 mini-presentation
